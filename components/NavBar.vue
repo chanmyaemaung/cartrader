dbf3071a6a77c291983b029962e87722e0eb536c
Chen Lay <maungmaungyoker.mmy@gmail.com>
2023-10-05 15:48:54 +0800

    :children_crossing: Convert link with NuxtLink

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,9 @@
   <!-- begin::Navbar -->
   <header class="w-full border-b bg-white">
     <nav class="sticky top-0 flex justify-between items-center space-x-1 p-4 shadow-md">
-      <a href="#" class="text-3xl font-mono">CarTrader</a>
+      <NuxtLink to="/">
+        <span class="text-3xl font-mono">CarTrader</span>
+      </NuxtLink>
     </nav>
   </header>
   <!-- end::Navbar -->
